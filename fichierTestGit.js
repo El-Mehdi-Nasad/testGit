1d314d20d11c59bff23cf8393980d9b6aab66df7
El-Mehdi-Nasad <nasad.ouarit.mehdi@gmail.com>
2020-03-22 17:09:36 +0100

    apres la creation du repertoire Ajouté des modif test sur mon fichier js fichierTestGit.js

--- fichierTestGit.js
+++ fichierTestGit.js
@@ -2,4 +2,9 @@ var test = 5;
 
 var test2 = test + 5;
 
-console.log ( test2 + "test resultat");
+console.log ( test2 + "test2 resultat");
+
+
+var test3 = test + test2;
+
+console.log ( test3 + "test3 resultat");
